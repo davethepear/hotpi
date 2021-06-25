b1d187d56eac14d01bae0d870c45543ff3a6f071
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-06-24 20:31:52 -0500

    Create temphot.sh

---
+++ temphot.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+tc=$(</sys/class/thermal/thermal_zone0/temp) # Most Pi CPU temp is located here
+tf=$(echo "scale=2;((9/5) * ($tc/1000)) + 32" |bc) # I know some C units, but I'm familiar with F.
+tx=$(echo "scale=2;($tc/1000)" |bc) # This was for something else I meant to add... you can delete it.
+# I set mine to whine when it's over 50°C, change if needed.
+if [ "$tc" -gt "50000" ]; then
+    echo "It's hot in here!" | mail -s "I am dying! $tf° F" myemail@host.com.net
+fi
