8428e73b2b7b88baf91f13c7ec218fc4b2d749eb
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-08-21 06:39:42 -0500

    Update temphot.sh

--- temphot.sh
+++ temphot.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 tc=$(</sys/class/thermal/thermal_zone0/temp) # Most Pi CPU temp is located here
 tf=$(echo "scale=2;((9/5) * ($tc/1000)) + 32" |bc) # I know some C units, but I'm familiar with F.
 tx=$(echo "scale=2;($tc/1000)" |bc) # This was for something else I meant to add... you can delete it.
