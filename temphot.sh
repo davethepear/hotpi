4ad2f7e4f7e5f03d6ed3f988336c418cd5d0b16a
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2022-02-20 17:36:16 -0600

    special characters removed

--- temphot.sh
+++ temphot.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+email=email@host.com
+max=54000 # The temperature reported by the pi, in C
 tc=$(</sys/class/thermal/thermal_zone0/temp) # Most Pi CPU temp is located here
 tf=$(echo "scale=2;((9/5) * ($tc/1000)) + 32" |bc) # I know some C units, but I'm familiar with F.
 tx=$(echo "scale=2;($tc/1000)" |bc) # This was for something else I meant to add... you can delete it.
 # I set mine to whine when it's over 50°C, change if needed.
 if [ "$tc" -gt "50000" ]; then
-    echo "It's hot in here!" | mail -s "I am dying! $tf° F" myemail@host.com.net
+    echo "CPU Temperature - Raw: $tc F: $tf C: $tx" | mail -s "$HOSTNAME is too hot!" $email
 fi
+exit 0
